077a231c9d1f1691ec4a75fd3e14c6ddf40eac56
toshi <k08045kk@gmail.com>
2021-04-30 12:30:50 +0900

    update https以外でもポップアップを表示する

--- src/js/popup.js
+++ src/js/popup.js
@@ -12,40 +12,39 @@
   function onInitialize() {
     const currentTab = {currentWindow:true, active:true};
     chrome.tabs.query(currentTab, function(tabs) {
-      const url = new URL(tabs[0].url);
-      if (url.protocol != 'https:') {
-        onComplate();
-        return;
+      let url;
+      try {
+        url = new URL(tabs[0].url);
+      } catch (e) {}
+      if (url && url.protocol == 'https:') {
+        chrome.runtime.sendMessage({
+          method: 'popup',
+          hostname: url.hostname,
+        }, function (response) {
+          if (response.data) {
+            document.getElementById('item_AddWhitelist').addEventListener('click', function() {
+              chrome.runtime.sendMessage({
+                method: 'addWhitelist',
+                hostname: url.hostname
+              });
+              onComplate();
+            });
+            document.getElementById('item_AddWhitelist').hidden = false;
+          } else {
+            document.getElementById('item_RemoveWhitelist').addEventListener('click', function() {
+              chrome.runtime.sendMessage({
+                method: 'removeWhitelist',
+                hostname: url.hostname
+              });
+              onComplate();
+            });
+            document.getElementById('item_RemoveWhitelist').hidden = false;
+          }
+        });
       }
-      
-      chrome.runtime.sendMessage({
-        method: 'popup',
-        hostname: url.hostname,
-      }, function (response) {
-        if (response.data) {
-          document.getElementById('item_AddWhitelist').addEventListener('click', function() {
-            chrome.runtime.sendMessage({
-              method: 'addWhitelist',
-              hostname: url.hostname
-            });
-            onComplate();
-          });
-          document.getElementById('item_AddWhitelist').style.display = '';
-          document.getElementById('popup').style.display = '';
-        } else {
-          document.getElementById('item_RemoveWhitelist').addEventListener('click', function() {
-            chrome.runtime.sendMessage({
-              method: 'removeWhitelist',
-              hostname: url.hostname
-            });
-            onComplate();
-          });
-          document.getElementById('item_RemoveWhitelist').style.display = '';
-          document.getElementById('popup').style.display = '';
-        }
-      });
+      document.getElementById('popup').hidden = false;
     });
   };
   
-  document.addEventListener("DOMContentLoaded", onInitialize);
+  document.addEventListener('DOMContentLoaded', onInitialize);
 })();
