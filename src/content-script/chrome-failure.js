e13ed0acf64eaff762b4a830631cb03bea356e18
toshi <k08045kk@gmail.com>
2023-12-27 00:49:27 +0900

    close #7 Manifest V3 対応
    close #12 Proxy について要検討

---
+++ src/content-script/chrome-failure.js
@@ -0,0 +1,29 @@
+// jsRejectServiceWorkerDisableKey
+navigator.serviceWorker.RejectServiceWorkerKey = '.';
+
+
+
+// jsRejectServiceWorker
+ServiceWorkerContainer.prototype.register = (scriptURL, options) => {
+  return Promise.reject(new Error('Reject to register a ServiceWorker.'));
+  // Issues #14: "Uncaught (in promise) Error: Reject to register a ServiceWorker."
+  // If the site does not catch it, the above error will occur.
+  // It will be described as an error on the extension side. (occurs from manifest v3)
+};
+
+
+
+// jsUnregisterServiceWorker
+navigator.serviceWorker.getRegistrations().then(registrations => {
+  for (let i=0; i<registrations.length; i++) {
+    registrations[i].unregister();
+    //console.log('RejectServiceWorker', 'unregister');
+  }
+  if (registrations.length != 0) {
+    window.caches.keys().then((keys) => {
+      Promise.all(keys.map((key) => window.caches.delete(key))).then(() => {
+        //console.log('RejectServiceWorker', 'caches delete');
+      });
+    });
+  }
+}).catch(error => {});
---
+++ src/content-script/firefox.js
@@ -0,0 +1,83 @@
+/**
+ * コンテンツスクリプト処理
+ * Firefox 用
+ */
+
+
+let verify = null;
+
+// サービスワーカー登録監視
+// サービスワーカー登録は、サービスワーカー登録鍵を待機する
+// サービスワーカー登録鍵あり時、登録する
+// サービスワーカー登録鍵なし時、登録拒否する
+const register = new Proxy(ServiceWorkerContainer.prototype.register, {
+  apply(target, self, args) {
+    return new window.Promise((resolve, reject) => {
+      const func = () => {
+        if (verify) {
+          if (verify === 'OK') {
+            Reflect.apply(target, self, args).then(resolve, reject);
+          } else {
+            reject(new window.Error('Reject to register a ServiceWorker.'));
+          }
+        } else {
+          window.setTimeout(func, 1000);
+        }
+      };
+      func();
+    });
+  }
+});
+// サービスワーカー登録拒否（再度、上書きされる可能性あり）
+const reject = function(scriptURL, options) {
+  return window.Promise.reject(new Error('Reject to register a ServiceWorker.'));
+};
+// サービスワーカー登録解除（失敗する可能性あり）
+const unregister = function() {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    for (let i=0; i<registrations.length; i++) {
+      registrations[i].unregister();
+      //console.log('ServiceWorker unregister.');
+    }
+    if (registrations.length != 0) {
+      caches.keys().then((keys) => {
+        Promise.all(keys.map((key) => caches.delete(key))).then(() => {
+          //console.log('caches delete.');
+        });
+      });
+    }
+  }).catch((error) => {});
+};
+// Note: #9 ページスクリプト内では、ページスクリプトのオブジェクトを使用する必要があります。
+//       window.Object でコンテンツスクリプトではなく、ページスクリプトのオブジェクトを使用できます。
+//       see https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Content_scripts
+
+
+
+// サービスワーカー登録監視
+exportFunction(
+    register, 
+    ServiceWorkerContainer.prototype, 
+    {defineAs: 'register'});
+
+
+// サービスワーカー登録許可・拒否
+chrome.storage.local.get(defaultStorage).then((cache) => {
+  if (cache.whitelist.includes(location.hostname)) {
+    // 許可
+    verify = 'OK';
+  } else {
+    // 拒否
+    verify = 'NG';
+    exportFunction(
+        reject, 
+        ServiceWorkerContainer.prototype, 
+        {defineAs: 'register'});
+    unregister();
+  }
+});
+
+
+// 備考
+// document_start でスクリプトを挿入することで、登録関数をバックアップ前に上書きできる予定
+// 登録鍵への総当り攻撃で登録をされる可能性がある。登録解除は、確実に成功する保証はない。
