b2f85a9a4e9bf6fcf44d29347a21d39c25d6cae5
toshi <k08045kk@gmail.com>
2024-03-13 23:05:27 +0900

    Firefox を mv2 に戻す #17, #18

--- src/common.js
+++ src/common.js
@@ -9,12 +9,14 @@
  *   manifest.json
  *     browser_specific_settings.gecko.strict_min_version = "115.0";
  *   109: Manifest V3 対応（既定で有効化）
+ *   115: chrome.storage.session
  *   115: ESR
  * 
  * Android Firefox 121.0+
  *   manifest.json
  *     browser_specific_settings.gecko_android.strict_min_version = "121.0";
  *   107: Android Firefox Bate: Supports WebExtension API
+ *   115: chrome.storage.session
  *   121: Android Firefox:      Supports WebExtension API
  *   122: Android Firefox: Bug: Obtaining host_permission is required for content_scripts operation.
  * 
@@ -29,3 +31,11 @@ const defaultStorage = {
   version: 2,
   whitelist: [],
 };
+
+try {
+  if (browser) {
+    // Firefox の mv2対応（chrome.〇〇 async / await 対策）
+    // Content script not working (Firefox) #17
+    chrome = browser;
+  }
+} catch {}
