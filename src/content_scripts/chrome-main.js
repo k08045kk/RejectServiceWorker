804a2d1ee178f65f498aa3334fbc3bfe987e7959
toshi <k08045kk@gmail.com>
2024-03-24 14:39:11 +0900

    コンテンツスクリプトの再構築（Chrome）
    script.addEventListener() を早期に設定する

--- src/content_scripts/chrome-main.js
+++ src/content_scripts/chrome-main.js
@@ -3,35 +3,36 @@
   const gError = Error;
   const gPromise = Promise;
   const gReflectApply = Reflect.apply;
-  const gAddEventListener = EventTarget.prototype.addEventListener;
   const gDispatchEvent = EventTarget.prototype.dispatchEvent;
   // Note: Modest resistance to apply / construct / getter traps.
   
   
   const name = 'RejectServiceWorker';
-  const event = new CustomEvent(name+'.ready');
   const script = document.currentScript;
   const key = script.dataset.arg;
   script.dataset.arg = '.';
   
   
-  let verifyPromise;
+  let ready = false;
+  const event = new CustomEvent(name+'.ready');
+  const verifyPromise = new Promise((resolve) => {
+    const listener = (event) => resolve(ready && event.detail === key ? 'OK' : 'NG');
+    script.addEventListener(name+'.verify', listener, {capture:true, onece:true, passive:true});
+    // Note: Without a ready event, receiving the event will fail due to timing issues.
+    //       It waits for the listener to register and then fires the verify event.
+    // Note: Don't verify until needed to reduce storage access.
+    //       If a security issue is confirmed, we will change the authentication method.
+  });
+  // Note: Authenticate with both dataset and addEventListener.
+  //       This is a risk hedge in case one of them is breached.
+  
+  
   ServiceWorkerContainer.prototype.register = new Proxy(ServiceWorkerContainer.prototype.register, {
     apply(target, self, args) {
       return new gPromise((resolve, reject) => {
-        if (!verifyPromise) {
-          verifyPromise = new gPromise((resolve) => {
-            const listener = (event) => resolve(event.detail === key ? 'OK' : 'NG');
-            const args = [name+'.verify', listener, {capture:true, onece:true, passive:true}];
-            gReflectApply(gAddEventListener, script, args);  // RejectServiceWorker.verify
-            gReflectApply(gDispatchEvent, script, [event]);  // RejectServiceWorker.ready
-            // Note: Without a ready event, receiving the event will fail due to timing issues.
-            //       It waits for the listener to register and then fires the verify event.
-            // Note: Don't verify until needed to reduce storage access.
-            //       If a security issue is confirmed, we will change the authentication method.
-          });
-          // Note: Authenticate with both dataset and addEventListener.
-          //       This is a risk hedge in case one of them is breached.
+        if (!ready) {
+          ready = true;
+          gReflectApply(gDispatchEvent, script, [event]);       // RejectServiceWorker.ready
         }
         verifyPromise.then((verify) => {
           if (verify === 'NG') {
